03420132c2fbe750027b19b580d046129e0f0a94
Joshua Lurz <jlurz24@gmail.com>
2013-06-09 20:51:59 -0400

    Get curves working and leash

---
+++ octave/add_gaussians.m
@@ -0,0 +1,56 @@
+function x, y = add_gaussians(xorig, yorig)
+
+  # Gaussian parameters
+  pNewGauss = 0.08;
+  timestep = 0.01;
+
+  # Store parameters for each gaussian
+  g = [];
+
+  # Iterate over timestep
+  for(i = 1:columns(xorig))
+    x(i) = xorig(i);
+    y(i) = yorig(i);
+
+    if(unifrnd(0,1) <= pNewGauss)
+      # Start a new gaussian
+      g(end + 1, :) = [unifrnd(-pi/4, pi/4), unifrnd(pi / 2, 2 * pi), i];
+    endif;
+
+    # Now iterate over all the gaussians.
+    for(gi = 1:rows(g))
+      # Start each gaussian shifted over to capture the full width.
+      gx = i - g(gi, 3) - 3 * g(gi, 2);
+      gy = g(gi, 1) * e^-(gx^2 / (2 * g(gi, 2)^2));
+
+      # Calculate a normal
+      if(i > 1)
+        dxy(1,1) = xorig(i) - xorig(i - 1);
+        dxy(1,2) = yorig(i) - yorig(i - 1);
+      else
+        dxy(1,1) = 0;
+        dxy(1,2) = 0;
+      end;
+
+      r(1, 1) = dxy(1,2);
+      r(1, 2) = -dxy(1,1);
+
+      # Now calculate the unit vector.
+      rl = sqrt(r(1, 1)^2 + r(1, 2)^2);
+      if(rl > 0)
+        ru = r / rl;
+      else
+        ru = r;
+      end;
+
+      # Increase the length
+      rn = ru * gy;
+
+      # Finally add the components
+      x(i) = x(i) + rn(1, 1);
+      y(i) = y(i) + rn(1, 2);
+    end;
+    
+    i++;
+  end;
+endfunction
---
+++ octave/lissajous.m
@@ -0,0 +1,19 @@
+function x, y = lissajous(tmax)
+
+  # Start by creating the lissajous curve.
+  x = [];
+  y = [];
+ 
+  # Lissajous parameters
+  a = sqrt(2);
+  delta = pi / 2;
+  A = 5;
+  B = 1.5;
+  b = 2 * a;
+
+  # Iterate over timestep
+  for ti = [0:0.01:tmax]
+    x(end + 1) = A * sin(a * ti + delta);
+    y(end + 1) = B * sin(b * ti);
+  end;
+endfunction
