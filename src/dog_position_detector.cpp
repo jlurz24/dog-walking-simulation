faffd144c67e8c282e139477cda1435a2c20a4d7
Joshua Lurz <jlurz24@gmail.com>
2013-07-24 20:58:25 -0400

    missing files

---
+++ src/dog_position_detector.cpp
@@ -0,0 +1,88 @@
+#include <ros/ros.h>
+#include <dogsim/utils.h>
+#include <tf/transform_listener.h>
+#include <gazebo_msgs/GetModelState.h>
+#include <dogsim/DogPosition.h>
+
+namespace {
+  using namespace std;
+  
+  class DogPositionDetector {
+    private:
+      //! Publisher for the dog position visualization.
+      ros::Publisher dogVizPub_;
+
+      //! Publisher for the dog positon
+      ros::Publisher dogPositionPub_;
+
+      //! Frequency at which to search for the dog
+      ros::Timer timer_;
+      
+      //! The node handle we'll be using
+      ros::NodeHandle nh_;
+
+      //! Private nh
+      ros::NodeHandle pnh_;
+
+      //! We will be listening to TF transforms
+      tf::TransformListener tf_;
+
+   public:
+      //! ROS node initialization
+      DogPositionDetector():pnh_("~"){
+        dogVizPub_ = nh_.advertise<visualization_msgs::Marker>("/dog_position_viz", 1);
+        dogPositionPub_ = nh_.advertise<dogsim::DogPosition>("/dog_position", 1);
+
+        ros::service::waitForService("/gazebo/get_model_state");
+        timer_ = nh_.createTimer(ros::Duration(0.1), &DogPositionDetector::callback, this);
+        timer_.start();
+      }
+
+   private:
+      void callback(const ros::TimerEvent& event){
+        // Lookup the current position.
+        if(!tf_.waitForTransform("/base_footprint", "/map", event.current_real, ros::Duration(0.25))){
+          ROS_WARN("Failed to get transform. Aborting iteration.");
+          return;
+        }
+        
+        geometry_msgs::PoseStamped dogPose;
+
+        // Lookup the current position of the dog.
+        ros::ServiceClient modelStateServ = nh_.serviceClient<gazebo_msgs::GetModelState>("/gazebo/get_model_state");
+        gazebo_msgs::GetModelState modelState;
+        modelState.request.model_name = "dog";
+        modelStateServ.call(modelState);
+        dogPose.header.stamp = event.current_real;
+        dogPose.header.frame_id = "/map";
+        dogPose.pose = modelState.response.pose;
+    
+        // Visualize the dog.
+        std_msgs::ColorRGBA BLUE = utils::createColor(0, 0, 1);
+        dogVizPub_.publish(utils::createMarker(dogPose.pose.position, dogPose.header, BLUE, true));
+
+        geometry_msgs::PoseStamped dogInBaseFrame;
+
+        // Determine the relative dog position
+        tf_.transformPose("/base_footprint", dogPose, dogInBaseFrame);
+        dogInBaseFrame.header.frame_id = "/base_footprint";
+        dogInBaseFrame.header.stamp = event.current_real;
+
+        // Publish the event
+        ROS_INFO("Publishing a dog position event");
+        dogsim::DogPosition dogPositionMsg;
+        dogPositionMsg.pose = dogInBaseFrame;
+        dogPositionMsg.header = dogInBaseFrame.header;
+        dogPositionPub_.publish(dogPositionMsg);
+      }
+  };
+}
+
+  int main(int argc, char** argv){
+    ros::init(argc, argv, "dog_position_detector");
+
+    DogPositionDetector positionDetector;
+    ros::spin();
+    ROS_INFO("Exiting Dog Position Detector");
+    return 0;
+  }
---
+++ src/get_path_server.cpp
@@ -0,0 +1,89 @@
+#include <ros/ros.h>
+#include <dogsim/GetPath.h>
+#include <boost/math/constants/constants.hpp>
+#include <geometry_msgs/Point.h>
+
+namespace {
+  using namespace ros;
+  using namespace std;
+
+  //! Factor to slow down the lissajous calculation.
+  static const double TIMESCALE_FACTOR = 10.0;
+
+  //! Amount of time it takes to perform a full lissajous cycle.
+  static const double LISSAJOUS_FULL_CYCLE_T = 4.60;
+
+  class GetPathServer {
+    private:
+      NodeHandle nh;
+	  ros::ServiceServer service;
+      bool started;
+      bool ended;
+      double startTime;
+    public:
+      GetPathServer():started(false), ended(false), startTime(0){
+        service = nh.advertiseService("/dogsim/get_path", &GetPathServer::getPath, this);
+      }
+    private:
+      geometry_msgs::PointStamped lissajous(const double t){
+
+        // Lissajous parameters.
+        static const double a = sqrt(2);
+        static const double delta = boost::math::constants::pi<long double>() / 2.0;
+        static const double A = 5.0;
+        static const double B = 1.5;
+        static const double b = 2 * a;
+
+        geometry_msgs::PointStamped goal;
+        goal.header.frame_id = "/map";
+        goal.point.x = -(A * sin(a * t + delta)) + 6.5; // Offset the start and invert;
+        goal.point.y = B * sin(b * t);
+        goal.point.z = 0.0;
+        return goal;
+      }
+
+      bool getPath(dogsim::GetPath::Request& req, dogsim::GetPath::Response& res){
+	    res.elapsedTime = req.time - startTime;
+        if(ended){
+          res.ended = true;
+          res.started = true;
+          return true;
+        }
+
+        if(!started){
+          if(!req.start){
+            // Not started yet.
+            res.ended = false;
+            res.started = false;
+            return true;
+          }
+
+          // First call. Initialize.
+          started = true;
+          startTime = req.time;
+		  ROS_INFO("Starting path @ time: %f", startTime);
+        }
+        else if((req.time - startTime) / TIMESCALE_FACTOR > LISSAJOUS_FULL_CYCLE_T){
+          ROS_INFO("Setting end flag @ time %f", req.time);
+          res.ended = ended = true;
+          res.started = true;
+          return true;
+        }
+
+        // TODO: Be smarter about making time scale based on velocity.
+        res.started = true;
+        res.ended = false;
+        res.point = lissajous((req.time - startTime) / TIMESCALE_FACTOR);
+		res.point.header.stamp = Time(req.time);
+        return true;
+      }
+  };
+}
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "get_path");
+  GetPathServer getPathServer;
+  ros::spin();
+  return 0;
+}
+
